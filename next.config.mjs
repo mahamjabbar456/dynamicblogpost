f899a374d545b323436c5272989696f21240ca51
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-11-27 09:54:18 +0500

    Modify

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images:{
         domains:["assets.aceternity.com","img.freepik.com","images.unsplash.com","plus.unsplash.com","media.istockphoto.com"]
     }
+    ,
+  eslint : {
+    ignoreDuringBuilds : true,
+}
 };
 
 export default nextConfig;
